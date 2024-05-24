094a3764d1c6df14439f74ead591ca112820fa98
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-05-24 19:35:15 +0700

    votes reducer & coba storybook

---
+++ src/stories/Button/Button.stories.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { fn } from '@storybook/test';
+import Button from './Button';
+
+export default {
+  title: 'Contoh/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: { onClick: fn() },
+};
+
+export const Primary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
+
+export const Warning = {
+  args: {
+    label: 'Delete now',
+    backgroundColor: 'red',
+  }
+};
